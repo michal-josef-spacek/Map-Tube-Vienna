78f292c019dee4aa295dac8cd139f00dd6c08ca9
Michal Josef Špaček <tupinek@gmail.com>
2025-01-23 10:06:23 +0100

    Improve LICENSE AND COPYRIGHT section in doc

--- Vienna.pm
+++ Vienna.pm
@@ -312,9 +312,11 @@ L<http://skim.cz>
 
 =head1 LICENSE AND COPYRIGHT
 
- © 2014-2025 Michal Josef Špaček
- Artistic License
- BSD 2-Clause License
+© 2014-2025 Michal Josef Špaček
+
+Artistic License
+
+BSD 2-Clause License
 
 =head1 VERSION
 
